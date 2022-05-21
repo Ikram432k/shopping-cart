54b056785a66ce592233f8bd17e9d05963a5c218
ikram mohideen <ikram432k@gmail.com>
2022-05-21 20:21:06 +0530

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Nav from './Components/navBar/navBar';
+import Home from './Components/home/home';
+import Shop from './Components/shop/shop';
+import About from './Components/about/about';
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+  <BrowserRouter>
+  <div>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/shop" element={<Shop/>} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+  </div>
+  </BrowserRouter>
+  )
 }
 
 export default App;
