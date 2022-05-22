c38338452a5a7fcc084d3019bfdf680a1ed7762d
ikram mohideen <ikram432k@gmail.com>
2022-05-22 09:19:40 +0530

    src/Components/home/homeStyle.js

--- src/Components/home/homeStyle.js
+++ src/Components/home/homeStyle.js
@@ -1,21 +1,54 @@
 import styled from "styled-components";
 import background from '../assets/shop-img.jpg'
 export const HomeStyles = styled.div`
-.home{}
-    // padding:50px;
+    flex: 1 1 0%;
     display: flex;
+    justify-content: center;
+    background-color: rgb(195, 195, 195);
+
+`;
+export const Div = styled.div`
+    flex: 1 1 0%;
+    display: flex;
+    justify-content: center;
     align-items:center;
-    justify-content:center;
+    flex-direction: column;
     background-image: url(${background});
-    // heigth:'100vh';
     background-size: cover;
+    color:white;
+    text-transform: uppercase;
 
+    h2{
+        background-color:black;
+        padding: 6px 8px 6px 14px;
+        font-size: 55px;
+        letter-spacing: 10px;
+    }
+    p{
+        font-size:14px;
+        font-weight:500px;
+        text-align:center;
+        letter-spacing: 7px;
+        line-height:1.5em;
+        max-width:100vh;
+        margin-top:15px;
+    }
+    button{
+        cursor: pointer;
+        text-transform: uppercase;
+        border:0;
+        color:white;
+        margin-top:25px;
+        background-color:black;
+        text-decoration: none;
+        padding: 6px 8px 6px 14px;
+        font-size:25px;
+        letter-spacing: 5px;
+        transition: 0.5s;
 
-    {
-        flex: 1 1 0%;
-        display: flex;
-        -webkit-box-pack: center;
-        justify-content: center;
-        background-color: rgb(195, 195, 195);
+        &:hover{
+            background-color:white;
+            color:black;
+        }
     }
 `;
