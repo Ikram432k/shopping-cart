7416fb3fa96beffb2940bf721e36417767c77d0a
ikram mohideen <ikram432k@gmail.com>
2022-05-22 09:19:11 +0530

    src/App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Nav from './Components/navBar/navBar';
 import Home from './Components/home/home';
 import Shop from './Components/shop/shop';
 import About from './Components/about/about';
+import styled from "styled-components";
 function App() {
+  const Div = styled.div`
+   display: flex; 
+   flex-direction: column;
+   min-height: 100vh;
+  `;
   return (
   <BrowserRouter>
-  <div>
+  <Div>
     <Nav />
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/shop" element={<Shop/>} />
       <Route path="/about" element={<About/>} />
     </Routes>
-  </div>
+  </Div>
   </BrowserRouter>
   )
 }
