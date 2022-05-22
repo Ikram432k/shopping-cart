40ba0efb06fae1379da142ad2fba41b18ff4589d
ikram mohideen <ikram432k@gmail.com>
2022-05-22 13:20:18 +0530

    src/Components/shop/shop.js

--- src/Components/shop/shop.js
+++ src/Components/shop/shop.js
@@ -1,6 +1,26 @@
+import {useState,useEffect} from 'react';
 function Shop(){
+useEffect(()=>{
+    fetchItems();
+},[]);
+
+const[items,setItems] =useState([]);
+
+const fetchItems = async()=>{
+    const data = await fetch('https://fakestoreapi.com/products')
+    const items =  await data.json();
+    setItems(items);
+    console.log(items);
+}
+
     return(
+        <div>
+
         <header>Products</header>
+            {items.map(item=>(
+                <h1>{item.title}</h1>
+            ))}
+        </div>
     )
 }
 export default Shop;
