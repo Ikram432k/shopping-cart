b9e8c56782d9bd0848dc28febc9630f883318998
ikram mohideen <ikram432k@gmail.com>
2022-05-23 22:06:58 +0530

    src/Components/cart/cart.js

--- src/Components/cart/cart.js
+++ src/Components/cart/cart.js
@@ -1,4 +1,45 @@
-function Cart(){
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 
+function Cart(){
+const { id } = useParams();
+const [item,setItem] = useState([]);
+useEffect(()=>{
+    fetchCart();
+},[])
+
+// const convertData =(data)=>{
+//     const {
+//         name: title,
+//         cost:price,
+//     } = data;
+//     return{
+//         title,price
+//     };
+// }
+
+const getItem =(data)=>{
+    let cloneItem = [...item];
+    cloneItem.push(data);
+    setItem(cloneItem);
+    console.log(item);
+}
+
+const fetchCart=async()=>{
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+    const data = await response.json();
+    getItem(data);
+    // console.log(item);
+}
+return(
+
+    <div>
+        <h2>your shopping cart</h2>
+        {/* <div className="cartItem">
+            <p>{item.title}</p>
+            <p>{}</p>
+        </div> */}
+    </div>
+)
 }
 export default Cart;
