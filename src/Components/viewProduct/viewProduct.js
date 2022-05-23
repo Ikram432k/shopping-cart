e88bd778e7d596940fb56bda398d335889722b10
ikram mohideen <ikram432k@gmail.com>
2022-05-23 22:07:58 +0530

    src/Components/viewProduct/viewProduct.js

--- src/Components/viewProduct/viewProduct.js
+++ src/Components/viewProduct/viewProduct.js
@@ -1,27 +1,82 @@
-import { Link } from "react-router-dom";
+import { Link,useParams } from "react-router-dom";
+import {useState,useEffect} from 'react';
+import {Div} from "./viewStyle"
 
 function ViewProduct(){
+    const { id } = useParams();
+
+const [product,setProduct] = useState({});
+const [rating,setRating] = useState({});
+const [count,setCount] = useState(1);
+useEffect(()=>{
+    fetchProduct();
+},[]);
+
+
+
+const fetchProduct = async()=>{
+    const data = await fetch(`https://fakestoreapi.com/products/${id}`)
+    const product = await data.json();
+    setProduct(product);
+    setRating(product.rating);
+    console.log(product);
+}
+
+const increase =()=>{
+    setCount(count+1);
+}
+const decrease =()=>{
+    if(count>0){
+        setCount(count-1);
+    }
+}
+const buyNow=()=>{
+    if(count>0){
+        alert("Thank you for your purchase");
+    }
+    else{
+        alert('The minimum qauntity of the purchase should be atlest one')
+    }
+}
+const handleCart=()=>{
+    // const temp = {
+    //     id:product.id,
+    //     quantity: count,
+    //     img: product.image,
+    //     title: product.title,
+    //     price: product.price,
+    //     };
+    //     props.getItems(temp);
+    }
+
     return(
-        <div>
-            <img src="" alt="img"/>
-            <div>
-                <p>product title</p>
-                <p>rating</p>
-                <p>stack</p>
-                <div>
-                    <button>+</button>
-                    <p>0</p>
-                    <button>-</button>
-                </div>
-                <p>0$</p>
-                <div>
-                    <Link to={'/cart'}>
-                    <button>add to cart</button>
+        <Div>
+            <div className="product">
+            <img src={product.image} alt={product.title}/>
+            <div className="productDetails">
+                <p>{product.title}</p>
+                <p>Rating:{rating.rate}/5</p>
+                <p>{product.description}</p>
+                {/* <div className="btn">
+                    <button onClick={decrease}>-</button>
+                    <p>{count}</p>
+                    <button onClick={increase}>+</button>
+                </div> */}
+                <p>Price:{product.price}$</p>
+                <div className="btns">
+                    <Link to={`/cart/${product.id}`}>
+                    <button onClick={handleCart}>add to cart</button>
                     </Link>
-                    <button>buy now</button>
+                    <Link to={'/shop'}>
+                    <button onClick={buyNow}>buy now</button>
+                    </Link>
+                    <Link to={'/shop'}>
+                    <button>go back</button>
+                    </Link>
                 </div>
             </div>
-        </div>
+            </div>
+        </Div>
     )
 }
 export default ViewProduct;
