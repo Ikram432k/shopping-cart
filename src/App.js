de134f70ae8a55746d205d434d7250dc98868c84
ikram mohideen <ikram432k@gmail.com>
2022-05-25 21:27:28 +0530

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Nav from './Components/navBar/navBar';
-import Home from './Components/home/home';
-import Shop from './Components/shop/shop';
-import About from './Components/about/about';
-import styled from "styled-components";
-import ViewProduct from "./Components/viewProduct/viewProduct";
-import Cart from "./Components/cart/cart";
+import Main from './Components/main/main'
+import './App.css';
 function App() {
-  const Div = styled.div`
-   display: flex; 
-   flex-direction: column;
-   min-height: 100vh;
-  `;
+
   return (
-  <BrowserRouter>
-  <Div>
-    <Nav />
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/shop" element={<Shop/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/viewproduct/:id" element={<ViewProduct />} />
-      <Route path="/cart/:id" element={<Cart/>} />
-    </Routes>
-  </Div>
-  </BrowserRouter>
+    <Main/>
   )
 }
 
