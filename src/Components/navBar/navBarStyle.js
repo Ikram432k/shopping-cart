d65088d0d4e9a98072b733eb7b12f5ad3e501bc6
ikram mohideen <ikram432k@gmail.com>
2022-05-25 21:31:23 +0530

    src/Components/navBar/navBarStyle.js

---
+++ src/Components/navBar/navBarStyle.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+export const Navs = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+background-color: rgb(45, 91, 189);
+color:  white;
+padding: 20px;
+text-transform: uppercase;
+
+    h1{
+        font-size: 2rem;
+
+    }
+    .nav-links{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        list-style-type: none;
+        width: 40vh;
+    
+    }
+    .cartIcon{
+        display: flex;
+    }
+    li{
+        cursor: pointer;
+        text-transform: uppercase;
+        border:0;
+        color:white;
+        margin-top:5px;
+        background-color:rgb(45, 91, 189);
+        padding:8px 10px;
+        font-size:15px;
+        // letter-spacing: 1px;
+        transition: 0.5s;
+        &:hover{
+            background-color:white;
+            color:black;
+        }
+    }
+`;
+
+// li{
+//     text-decoration: none;
+//     color:"white"
+    
+// }
