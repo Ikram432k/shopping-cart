e1888eeddd333492107f415996ac69e75c4ee2d7
ikram mohideen <ikram432k@gmail.com>
2022-05-26 09:49:38 +0530

    src/Components/home/home.js

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -5,8 +5,8 @@ function Home(){
     return(
         <HomeStyles>
         <Div>
-        <h2>Fake Fashion</h2>
-        <p>Welcome,to the fake fashion. The online shopping interface that has varity of products.</p>
+        <h2>Fake store</h2>
+        <p>Welcome,to the fake store. The online shopping interface that has varity of products.</p>
         <Link to={'/shop'}>
         <button>SHOP now</button>
         </Link>
